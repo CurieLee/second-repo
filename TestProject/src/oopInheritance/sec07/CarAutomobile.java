a71cf96aff10baa1b9e88585d4ed660e62dca4b7
Curie Lee <dlrbfl98@daum.net>
2025-07-30 22:20:20 +0900

    Commit extendsEx 자동차 정보 출력

---
+++ TestProject/src/oopInheritance/sec07/CarAutomobile.java
@@ -0,0 +1,8 @@
+package oopInheritance.sec07;
+
+public class CarAutomobile {
+	public static void main(String[] args) {
+		Automobile auto = new Automobile("01가1234", "아반떼", "현대", 2022, "자동");
+		System.out.print(auto);
+	}
+}
