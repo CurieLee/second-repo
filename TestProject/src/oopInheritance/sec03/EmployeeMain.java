1c5ba546e5dd16fa3cb9765dcfa4d82f46ccf158
Curie Lee <dlrbfl98@daum.net>
2025-07-30 22:20:51 +0900

    Commit Java Study @Override: toString()

---
+++ TestProject/src/oopInheritance/sec03/EmployeeMain.java
@@ -0,0 +1,14 @@
+package oopInheritance.sec03;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee emp1 = new Employee("1234", "홍길동", "마케팅");
+		Employee emp2 = new Employee("5678", "이몽룡", "영업");
+		
+		System.out.println(emp1);
+		System.out.println(emp2);
+	}
+
+}
---
+++ TestProject/src/oopInheritance/sec04/EmpMain.java
@@ -0,0 +1,12 @@
+package oopInheritance.sec04;
+
+public class EmpMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Manager mg = new Manager("1234", "홍길동", "마케팅", "사원");
+		System.out.println(mg);
+		
+	}
+
+}
