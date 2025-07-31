a8580bea250d33d45826a29699edceda11ac9984
Curie Lee <dlrbfl98@daum.net>
2025-07-31 14:47:36 +0900

    Update Java Study 다형성

--- TestProject/src/oopInheritance/sec07/PartTimeMain.java
+++ TestProject/src/oopInheritance/sec07/PartTimeMain.java
@@ -4,5 +4,8 @@ public class PartTimeMain {
 	public static void main(String[] args) {
 		PartTime pt = new PartTime("991111-1234567", "홍길동", 60, 15000);
 		System.out.println(pt);
+		
+		Worker wk = pt; // sub class 객체 참조를 super class 참조변수에 대입
+		System.out.println(wk);
 	}
 }
