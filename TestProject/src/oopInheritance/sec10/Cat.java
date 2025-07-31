374ce39183e46fa7ed61f45dbbbf016b671b44de
Curie Lee <dlrbfl98@daum.net>
2025-07-31 14:46:54 +0900

    Commit Java Study @Override method

---
+++ TestProject/src/oopInheritance/sec10/Cat.java
@@ -0,0 +1,11 @@
+package oopInheritance.sec10;
+
+public class Cat extends Animal {
+	public void show() {
+		System.out.println("Cat");
+	}
+	
+	public void sound() {
+		System.out.println("Meow Meow Meow");
+	}
+}
---
+++ TestProject/src/oopInheritance/sec10/Dog.java
@@ -0,0 +1,11 @@
+package oopInheritance.sec10;
+
+public class Dog extends Animal {
+	public void show() {
+		System.out.println("Dog");
+	}
+	
+	public void sound() {
+		System.out.println("Bark Bark Bark");
+	}
+}
