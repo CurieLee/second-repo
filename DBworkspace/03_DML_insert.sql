44719fb4cc9531365552017d03537ac0d7b8bf1b
Curie Lee <dlrbfl98@daum.net>
2025-08-08 17:48:08 +0900

    Commit SQL DML insert

--- DBworkspace/03_DML_insert.sql
+++ DBworkspace/03_DML_insert.sql
@@ -10,3 +10,72 @@
 INSERT INTO STUDENT (stdNo, stdName, grade, deptNo)
 VALUES ('20150000', '변학도', 4, '1');
 -- 값 문자열 홑따옴표
+
+
+
+
+
+
+
+--------------------------------------------------------------------------------
+-- 시퀀스
+-- 오라클 데이터베이스 객체로 유일한 값의 일련번호 생성
+-- 지정한 수치로 증가하거나 감소
+-- 기본키에 자동 증가값을 사용할때 유용
+-- 최대 15개까지 생성 가능
+-- 테이블과 독립적으로 저장 생성
+-- 하나의 시퀀스 여러 테이블에서 사용 가능
+
+--  시퀀스 적용할 테이블 생성
+DROP TABLE board;
+CREATE TABLE  board(
+    bNo NUMBER PRIMARY KEY,
+    bSubject VARCHAR2(30) NOT NULL,
+    bname   VARCHAR2(20) NOT NULL
+    );
+    
+CREATE SEQUENCE NO_SEQ;
+
+-- 데이터 추가
+INSERT INTO board VALUES(NO_SEQ.NEXTVAL, '추석', '홍길동');
+INSERT INTO board VALUES(NO_SEQ.NEXTVAL, '미세먼지', '홍길동');
+INSERT INTO board VALUES(NO_SEQ.NEXTVAL, '휴가', '홍길동');
+
+SELECT * FROM BOARD;
+
+-- 시퀀스 값 검색
+SELECT NO_SEQ.CURRVAL FROM DUAL;
+
+-- 시퀀스 수정
+ALTER SEQUENCE NO_SEQ
+    MAXVALUE 1000;
+    
+-- 수정 결과 검색: 구조에 대한 검색, 시퀀스 정보 저장 테이블 USER_SEQUENCES
+SELECT SEQUENCE_NAME, MAX_VALUE
+FROM USER_SEQUENCES;
+
+-- 시퀀스 삭제: DROPO SEQUENCE 시퀀스명
+DROP SEQUENCE NO_SEQ;
+
+SELECT SEQUENCE_NAME FROM USER_SEQUENCES;
+
+-- DUAL 테이블
+-- 오라클 자체에서 제공되는 테이블
+-- 1개의 행과 1개의 열만 있는 더미 테이블
+-- SYS 소유지만 모든 사용자가 사용할 수 있음
+-- 간단한 함수 계산 결과값 확인용
+SELECT CURRENT_DATE FROM DUAL;
+
+--------------------------------------------------------------------------------
+-- data import
+-- 텍스트 파일을 읽어서 테이블 생성 및 데이터 구성
+-- 제약조건 추가
+ALTER TABLE PRODUCT
+	ADD CONSTRAINT PK_PRODUCT_PRDNO
+    PRIMARY KEY(PRDNO);
+
+
+
+
+
+    
