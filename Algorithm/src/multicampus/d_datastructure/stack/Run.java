64de0d8e19da6562419d3f8f8bd931ca9602408e
Curie Lee <dlrbfl98@daum.net>
2025-08-24 23:25:32 +0900

    Commit Algorithm data structure Stack

---
+++ Algorithm/src/multicampus/d_datastructure/stack/Run.java
@@ -0,0 +1,38 @@
+package multicampus.d_datastructure.stack;
+
+import multicampus.d_datastructure.map._HashMap;
+
+public class Run {
+
+	public static void main(String[] args) {
+		System.out.println(isPair("(){[]{}}"));
+		System.out.println(isPair("((){[]{}}"));
+		
+		System.out.println('(' + ')');
+		System.out.println('{' + '}');
+		System.out.println('[' + ']');
+	}
+	
+	private static boolean isPair(String text) {
+		
+		_Stack<Character> stack = new _Stack<Character>();
+		
+		for (char ch : text.toCharArray()) {
+			
+			if ("({[".contains(String.valueOf(ch))) {
+				stack.push(ch);
+				continue;
+			};
+			
+			if (!")}]".contains(String.valueOf(ch)))	continue;
+			if (stack.isEmpty())	return false;
+			
+			int k = stack.pop();
+			if (ch + k != 81 && ch + k != 184 && ch + k != 248) return false;
+		}
+		
+		return stack.isEmpty();
+	}
+	
+
+}
